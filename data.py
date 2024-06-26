44b7fa6a45e0998daa77214f49d2e82b95fe7a01
JayPip <jakub.pipien@gmail.com>
2024-06-26 19:03:50 +0200

    Generate csv files, normalize data and find the most similiar epitopes

--- data.py
+++ data.py
@@ -1,4 +1,6 @@
 import json
+import csv
+import numpy
 from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 
@@ -15,13 +17,28 @@ def extract_sequences(data):
         sequences.append((organism, sequence))
     return sequences
 
-def compare_sequences(sequences1, sequences2):
+def compare_sequences(sequences_human, sequences_mouse):
     comparisons = []
-    for seq1 in sequences1:
-        for seq2 in sequences2:
+    for seq1 in sequences_human:
+        for seq2 in sequences_mouse:
             alignments = pairwise2.align.globalxx(seq1[1], seq2[1])
-            comparisons.append((seq1, seq2, format_alignment(*alignments[0])))
+            #normalize data
+            comparisons.append({(alignments[0].score / min(len(seq1[1]), len(seq2[1]))), seq1[1], seq2[1]})
     return comparisons
+
+def perform_permutation(sequences_human, sequences_mouse, num_permutations=50):
+    scores = []
+    for seq1 in sequences_human:
+        for seq2 in sequences_mouse:
+            scores_list = []
+            for _ in range(num_permutations):
+                permuted_seq1 = permute_sequence(seq1[1])
+                permuted_seq2 = permute_sequence(seq2[1])
+                permuted_scores = pairwise2.align.globalxx(permuted_seq1, permuted_seq2)
+                scores_list.append((permuted_scores[0].score/ min(len(seq1[1]), len(seq2[1]))))
+            scores.append(scores_list)
+
+    return scores
 
 def print_comparisons(comparisons,x):
     f = open("output/" + x + "out.txt", "w")
@@ -33,6 +50,15 @@ def print_comparisons(comparisons,x):
         f.write(f"{alignement}\n")
     f.close()
 
+def createCsvFile(x, comparisons):
+    with open("output/" + x + "out.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(['scores'])
+        writer.writerows(comparisons)
+
+def permute_sequence(seq):
+    return ''.join(numpy.random.permutation(list(seq)))
+
 def main():
     print('Enter protein name:')
     x = input()
@@ -42,8 +68,21 @@ def main():
     data_mouse = load_json(mouse_file_path)
     sequences_human = extract_sequences(data_human)
     sequences_mouse = extract_sequences(data_mouse)
-    comparisons = compare_sequences(sequences_human, sequences_mouse)
-    print_comparisons(comparisons, x)
+
+    comparisons_orignal = compare_sequences(sequences_human, sequences_mouse)
+    # createCsvFile(x, comparisons_orignal)
+
+    max_alignment = {}
+    maxFloat = 0;
+    for item in comparisons_orignal:
+        for s in item:
+            if isinstance(s, float):
+                if s > maxFloat:
+                    max_alignment = item
+                    maxFloat = s
+
+
+    print(max_alignment)
 
 if __name__ == "__main__":
     main()
